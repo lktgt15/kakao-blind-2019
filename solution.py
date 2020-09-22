c7a223727c30d30e2a7ec1b02dbd3b6d6012041e
lktgt15 <64021540+lktgt15@users.noreply.github.com>
2020-09-23 03:20:26 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,6 @@ elvs = 4
 
 startStat = elv.start(user_key, problem_id, elvs)
 token = startStat['token']
-# token = 'KHZby'
 
 print(token)
 
@@ -58,32 +57,34 @@ def solve():
             ElvStat = Elevator['status']
             ElvFloor = Elevator['floor']
             ElvPassengers = Elevator['passengers']
+
             fcall_end = floorCall(ElvFloor,ElvPassengers,False)
             fcall_enter = floorCall(ElvFloor,oncalls,True)
+
             cmd = {}
             cmd['elevator_id'] = i
 
-            passengers = upOrdown(ElvFloor,ElvPassengers)
-            enters = upOrdown(ElvFloor,fcall_enter)
-            calls = upOrdown(ElvFloor,oncalls,False)
+            ElvPassengersDir = upOrdown(ElvFloor,ElvPassengers)
+            fcall_enterDir = upOrdown(ElvFloor,fcall_enter)
+            oncallsDir = upOrdown(ElvFloor,oncalls,False)
 
             if ElvStat == 'STOPPED':
                 #OPEN
                 if fcall_end: # 내리는 사람이 있다.
                     cmd['command'] = 'OPEN'
                 elif fcall_enter and len(ElvPassengers) < 8: # 타려는 사람이 있고 엘리베이터에 수용된다.
-                    if not passengers or passengers[0] in enters: # 타려는 사람이 엘리베이터에 타있는 사람과 방향이 같다.
+                    if not ElvPassengersDir or ElvPassengersDir[0] in fcall_enterDir: # 타려는 사람이 엘리베이터에 타있는 사람과 방향이 같다.
                         cmd['command'] = 'OPEN'
                     else: # 타려는 사람이 엘리베이터에 타있는 사람과 방향이 같지않다.
-                        if passengers: # 현재 고객이 있다.
-                            cmd['command'] = passengers[0]
+                        if ElvPassengersDir: # 현재 고객이 있다.
+                            cmd['command'] = ElvPassengersDir[0]
                         else: # 현재 고객이 없다.
-                            cmd['command'] = calls[0]
+                            cmd['command'] = oncallsDir[0]
                 else: # 타려는 사람이 없거나 엘리베이터에 수용할 수 없다.
-                    if passengers:
-                        cmd['command'] = passengers[0]
-                    elif calls:
-                        cmd['command'] = calls[0]
+                    if ElvPassengersDir:
+                        cmd['command'] = ElvPassengersDir[0]
+                    elif oncallsDir:
+                        cmd['command'] = oncallsDir[0]
                     else:
                         cmd['command'] = 'STOP'
             elif ElvStat == 'OPENED':
@@ -94,16 +95,11 @@ def solve():
                         cmd['call_ids'].append(Call['id'])
                         allcalls.append(Call['id'])
                 elif fcall_enter and len(ElvPassengers) < 8: # 타려는 사람이 있고 엘리베이터에 수용된다.
-                    if not passengers or passengers[0] in enters: # 타려는 사람이 타있는 사람과 방향이 같다.
+                    if not ElvPassengersDir or ElvPassengersDir[0] in fcall_enterDir: # 타려는 사람이 타있는 사람과 방향이 같다.
                         cmd['command'] = 'ENTER'
                         cmd['call_ids'] = []
                         for j, Call in enumerate(fcall_enter):
-                            tmp = 0
-                            if not passengers:
-                                tmp = 0
-                            else:
-                                tmp = passengers[0]
-                            if len(cmd['call_ids']) + len(ElvPassengers) < 8 and (not passengers or enters[j] == passengers[0]) and Call['id'] not in allcalls:
+                            if len(cmd['call_ids']) + len(ElvPassengers) < 8 and (not ElvPassengersDir or fcall_enterDir[j] == ElvPassengersDir[0]) and Call['id'] not in allcalls:
                                 cmd['call_ids'].append(Call['id'])
                                 allcalls.append(Call['id'])
                     else:
@@ -114,10 +110,10 @@ def solve():
                 if fcall_end: # 내리는 사람이 있다.
                     cmd['command'] = 'STOP'
                 elif fcall_enter and len(ElvPassengers) < 8: # 타려는 사람이 있고 엘리베이터에 수용된다.
-                    if not passengers or passengers[0] in enters: # 타있는 사람과 타려는 사람의 방향이 같다.
+                    if not ElvPassengersDir or ElvPassengersDir[0] in fcall_enterDir: # 타있는 사람과 타려는 사람의 방향이 같다.
                         cmd['command'] = 'STOP'
                     else:
-                        if passengers[0] == 'DOWN':
+                        if ElvPassengersDir[0] == 'DOWN':
                             cmd['command'] = 'STOP'
                         else:
                             cmd['command'] = 'UP'
@@ -127,10 +123,10 @@ def solve():
                 if fcall_end:
                     cmd['command'] = 'STOP'
                 elif fcall_enter and len(ElvPassengers) < 8:
-                    if not passengers or passengers[0] in enters:
+                    if not ElvPassengersDir or ElvPassengersDir[0] in fcall_enterDir:
                         cmd['command'] = 'STOP'
                     else:
-                        if passengers[0] == 'UP':
+                        if ElvPassengersDir[0] == 'UP':
                             cmd['command'] = 'STOP'
                         else:
                             cmd['command'] = 'DOWN'
